be095260916840bcf306e111f7b0815b27fb9e94
myd2898129 <myd2898129@outlook.com>
2023-02-17 18:04:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+# git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-wolplus
+# git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/myd2898129/openwrt-packages package/kiddin9-packages
