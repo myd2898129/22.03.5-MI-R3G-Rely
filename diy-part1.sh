1b2c00a16089b8cc897d6fa8a8d86b6eedee80c7
myd2898129 <myd2898129@outlook.com>
2023-03-14 22:31:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-# git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-wolplus
-git clone https://github.com/myd2898129/openwrt-packages package/kiddin9-packages
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8-packages
